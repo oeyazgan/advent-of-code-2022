3d9819321b3298c49260cf4c1a7af3b6308d6d36
oeyazgan <onuremreyazgan@gmail.com>
2022-12-06 12:03:24 +0100

    solve day 6

--- src/aoc_2022/core.clj
+++ src/aoc_2022/core.clj
@@ -199,4 +199,16 @@
   [(apply str (with-move-func move-single))
    (apply str (with-move-func move-multiple))])
 
-(day-5)
+;; day-6
+(defn with-partition [prt]
+  (->> (slurp "resources/day-6.txt")
+       (partition prt 1)
+       (map (comp count #(apply hash-set %)))
+       (map-indexed vector)
+       (drop-while (fn [[x y]] (not= y prt)))
+       (#((comp (partial + prt) first first) %))))
+
+(defn day-6 []
+  [(with-partition 4) (with-partition 14)])
+
+(day-6)
